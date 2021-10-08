2fda1426729a6ddc416d8a47a289f641cb625060
Luma Montes <lumagoesmontes@gmail.com>
2021-10-08 16:44:12 -0300

    adicionado tratamento pra erros gerais

--- server/index.js
+++ server/index.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const routes = require('./route/imobbilesRoute')
+
 const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.use((error, req, res, next ) => {
+    res.status(error.status || 500)
+    res.json({error: error.message})
+})
+
+// app.use('/', require('./route/imobbilesRoute'));
 
 app.listen(3000);
