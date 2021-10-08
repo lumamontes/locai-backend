ebf29e7add83f68a18786c9d45070887dc126d25
Luma Montes <lumagoesmontes@gmail.com>
2021-10-08 17:00:54 -0300

    finalizado endpoint para criar novo imovel

--- server/controllers/ImobbilesController.js
+++ server/controllers/ImobbilesController.js
@@ -8,7 +8,41 @@ module.exports = {
                 return response.json(error);               
             }
         },
-
+        async create(request, response, next) {
+            try{
+                const {
+                    imobbile_type_id,
+                    user_id,
+                    ad_title,
+                    ad_description,
+                    ad_value,
+                    room_quantity,
+                    bathroom_quantity,
+                    imobbile_adress,
+                    imobbile_country,
+                    imobbile_state,
+                    with_furniture,
+                    accepts_pets,
+                 } = request.body;
+                await knex('imobbiles').insert({
+                    imobbile_type_id,
+                    user_id,
+                    ad_title,
+                    ad_description,
+                    ad_value,
+                    room_quantity,
+                    bathroom_quantity,
+                    imobbile_adress,
+                    imobbile_country,
+                    imobbile_state,
+                    with_furniture,
+                    accepts_pets,      
+                });
+                return response.status(201).send('status: Imovel cadastrado com sucesso');
+            }catch(error){
+                    next(error);               
+             }
+        }
 }
 
 
