7e8796428f4b41deeea90c484ebefb204cbb1922
Luma Montes <lumagoesmontes@gmail.com>
2021-10-09 00:11:00 -0300

    adicionado tratamento para erro de rota nao encontrada

--- server/index.js
+++ server/index.js
@@ -6,6 +6,14 @@ const app = express();
 app.use(express.json());
 app.use('/api', routes);
 
+//not Found                          
+app.use((req, res, next ) => {
+    const error = new Error('Not found');
+    error.status = 404
+    next(error);
+})
+
+// catch all
 app.use((error, req, res, next ) => {
     res.status(error.status || 500)
     res.json({error: error.message})
