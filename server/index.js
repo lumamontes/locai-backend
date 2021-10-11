d2c81383d0669698a69ef36a04fc9227b95a9e4b
Luma Montes <lumagoesmontes@gmail.com>
2021-10-11 19:20:22 -0300

    corrigido cors

--- server/index.js
+++ server/index.js
@@ -1,25 +1,21 @@
 const express = require('express');
+const cors = require('cors');
+
 const imobbilesRoutes = require('./route/imobbilesRoute')
 const imobbilesTypesRoute = require('./route/imobbilesTypesRoute')
 const usersControllerRoute = require('./route/UsersRoute')
-
-const cors = require('cors');
-
 const app = express();
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
 app.use((req, res, next) => {
-    //console.log("Acessou o Middleware!");
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
     next();
 });
-
-app.get('/', (req, res) => {
-	return res.json({ status: true }).send();
-})
 
 app.use('/api', imobbilesRoutes);
 app.use('/api', imobbilesTypesRoute);
