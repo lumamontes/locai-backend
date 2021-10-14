2969975a4af1076502f4b39b66086042c94a4699
Luma Montes <lumagoesmontes@gmail.com>
2021-10-13 21:04:04 -0300

    criado metodo para gerar token de autenticaçao do usuario ao realizar login

--- server/controllers/UsersController.js
+++ server/controllers/UsersController.js
@@ -1,14 +1,19 @@
 const knex = require('../../database/knex');
 const jwt = require('jsonwebtoken');
+const decode = require('jwt-decode');
 const bcrypt = require('bcryptjs');
+const passport = require("passport");
+const uuid =  require('uuid').v4;
+const tokens = ['', ''];
 
 module.exports = {
+    
     async index(request, response) {
         try {
             const { id } = request.params;
             let users = await knex.from('users')
-            .where({id})
-                
+                .where({ id })
+
             return response.json(users);
         } catch (error) {
             return response.json(error);
@@ -44,10 +49,142 @@ module.exports = {
                 profile_picture,
                 password: hashedPassword
             });
+            // let password = hashedPassword;
+
             return response.status(201).send();
         } catch (error) {
             next(error);
         }
+    },
+    async sessions(request, response, next) {
+
+        function generateJwtAndRefreshToken(email, payload = {}) {
+            const token = jwt.sign(payload, 'supersecret', {
+            subject: email,
+            expiresIn: 5, // 15 minutes
+            });
+            const refreshToken = createRefreshToken(email, token)
+        
+            return {
+            token,
+            refreshToken,
+            }
+        }
+
+        function createRefreshToken(email, token){
+            const currentUserTokens = [];
+            const refreshToken = uuid();
+            tokens.push(email, [...currentUserTokens, refreshToken])
+            return refreshToken;
+          }
+        function checkRefreshTokenIsValid(email,refreshToken){
+            const storedRefreshTokens = tokens.get(email)??[]
+          
+            return storedRefreshTokens.some(token => token == refreshToken)
+          }
+          
+          function invalidateRefreshToken(email, refreshToken) {
+            const storedRefreshTokens = tokens.get(email) ?? []
+          
+            tokens.set(email, storedRefreshTokens.filter(token => token !== refreshToken));
+          }
+
+        const {
+            email,
+            password
+        } = request.body;
+        let users = await knex.from('users')
+        .where({ email })
+        if (!users) {
+            return response.
+            status(401)
+            .json({
+                error: true,
+                message: 'Não foi possível encontrar o usuário'
+            });
+        }
+        for(user of users){
+            try{
+                if(await bcrypt.compare(password, user.password)){
+                    
+                    const { token, refreshToken } = generateJwtAndRefreshToken(email, {
+                        permissions: user.permissions,
+                    })
+                    return response.json({
+                        token,
+                        refreshToken,
+                        user_type_id: user.user_type_id,
+                    })
+                }else{
+                    return response.
+                        status(401)
+                        .json({
+                            error: true,
+                            message: 'Senha incorreta'
+                        });
+                }
+            }catch(err){
+                console.log('fora');
+                next(err);
+            }
+        }
+
+        // const { token, refreshToken } = generateJwTandRefreshToken(email, {
+        //     permissions: user.permissions,
+        // })
+        // // console.log(request.body);
+        // // console.log(token, refreshToken)
+
+        // return response.json({
+        //     token,
+        //     refreshToken,
+        //     permissions: user.user_type_id,
+        // })
+
+    },
+    async addUserInformationToRequest(request, response, next) {
+        const email = request.user;
+        // const { refreshToken } = request.body;
+        let user = await knex.from('users')
+            .where({ email })
+        if (!user) {
+            return response.
+                status(401)
+                .json({
+                    error: true,
+                    message: 'Usuário não encontrado'
+                });
+        }
+
+        const { token, refreshToken } = generateJwTandRefreshToken(email, {
+            permissions: user.permissions,
+        })
+
+        return response.status(200).json({
+            token,
+            refreshToken,
+            permissions: user.user_type_id,
+        })
+
+    },
+    async me(request, response, next) {
+        const email = request.user;
+        let user = await knex.from('users')
+            .where({ email })
+        if (!user) {
+            return response.
+                status(401)
+                .json({
+                    error: true,
+                    message: 'Usuário não encontrado'
+                });
+        }
+        return response.json({
+            token,
+            refreshToken,
+            permissions: user.user_type_id,
+        })
+
     },
 }
 
