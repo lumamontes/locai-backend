bb1b75ff440827409a154eb326eb35c0f6869c22
Luma Montes <lumagoesmontes@gmail.com>
2021-10-13 21:01:05 -0300

    criado novas rotas para usuarios

--- server/route/UsersRoute.js
+++ server/route/UsersRoute.js
@@ -4,5 +4,8 @@ const UsersController = require('../controllers/UsersController');
 
 router.get('/users/:id', UsersController.index);
 router.post('/users', UsersController.create);
+router.post('/login', UsersController.sessions);
+router.post('/refresh', UsersController.addUserInformationToRequest);
+router.get('/me', UsersController.me);
 
 module.exports = router;
