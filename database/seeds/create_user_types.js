1176b51ab77b84e2b5ab8fd78eb703baf4693fc5
Luma Montes <lumagoesmontes@gmail.com>
2022-01-26 21:36:55 -0300

    corrigido migration

--- database/seeds/create_user_types.js
+++ database/seeds/create_user_types.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('property_types').del()
+  return knex('user_types').del()
     .then(function () {
       // Inserts seed entries
-      return knex('property_types').insert([
+      return knex('user_types').insert([
         {id: 1, name: 'comprador', created_at: knex.fn.now(), updated_at: knex.fn.now()},
         {id: 2, name: 'anunciante',created_at: knex.fn.now(), updated_at: knex.fn.now()},
         {id: 3, name: 'corretor', created_at: knex.fn.now(), updated_at: knex.fn.now()}
