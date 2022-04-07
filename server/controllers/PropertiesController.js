b077f70308f33d4bdabc08b962767c229e5c37d0
Luma Montes <lumagoesmontes@gmail.com>
2022-04-07 01:55:27 -0300

    melhorado consultas de propriedades e adicionado logica de novo campo para imagem

--- server/controllers/PropertiesController.js
+++ server/controllers/PropertiesController.js
@@ -15,112 +15,37 @@ module.exports = {
     async single_read(request, response) {
         try {
             const { id } = request.params;
-            let properties = await knex.from('properties')
-                .where({ id })
-
-            return response.json(properties);
-        } catch (error) {
-            return response.json(error);
-        }
-    },
-    async single_user(request, response) {
-        try {
-            const { user_id } = request.params;
-            let properties = await knex.from('properties')
-                .where({ user_id })
-            return response.json(properties);
-        } catch (error) {
-            return response.json(error);
-        }
-    },
-    async create(request, response, next) {
-        const {
-            property_type_id,
-            category_id,
-            user_id,
-            ad_title,
-            ad_description,
-            ad_value,
-            room_quantity,
-            bathroom_quantity,
-            garage_quantity,
-            property_adress,
-            property_country,
-            property_city,
-            property_state,
-            property_neighborhood,
-            with_furniture,
-            accepts_pets,
-        } = request.body;
-        let users = await knex.from('users')
-            .where('id', user_id);
-        if (users.length == 0) {
-            return response.
-                status(400)
-                .json({
-                    error: true,
-                    message: 'Usuário inválido'
-                });
-        } else {
-            try {
-                let teste = await knex('properties').insert({
-                    property_type_id,
-                    category_id,
-                    user_id,
-                    ad_title,
-                    ad_description,
-                    ad_value,
-                    room_quantity,
-                    bathroom_quantity,
-                    garage_quantity,
-                    property_adress,
-                    property_country,
-                    property_neighborhood,
-                    property_city,
-                    property_state,
-                    with_furniture,
-                    accepts_pets,
-                })
-                    .returning('id')
-                    .then(async id => {
-                        for (let i = 0; i < request.files.length; i++) {
-                            try {
-                                const url = await imgur.uploadFile(`./tmp/uploads/${request.files[i].filename}`);
-                                await knex('files').insert({
-                                    property_id: id[0],
-                                    url: url.link,
-                                })
-                                if (request.files[0]) {
-                                    try {
-                                        await knex('properties').where('id', '=', id[0]).update({
-                                            ad_image: url.link
-                                        })
-                                    } catch (err) {
-                                        console.log(error);
-                                    }
-                                };
-                                fs.unlinkSync(`./tmp/uploads/${request.files[i].filename}`);
-                            } catch (error) {
-                                return response.status(400).json({
-                                    message: error.message,
-                                });
-                            }
-                        }
-                        return response.status(201).json({
-                            property_id: id[0],
-                            message: 'Cadastro com sucesso!'
-                        });
+            await knex.select()
+            .from('properties').where({ id })
+            .then(async function(properties){
+               await knex.select('url')
+                .from('files').where('property_id', id)
+                .then(function(files) {
+                    response.json({
+                            property: properties,
+                            files: files,
                     });
-            } catch (error) {
-                return response.json({
-                    message: error.message,
-                })
-            }
-
+                });
+            }).catch(function(error) {
+                console.log(error);
+            });
+        }catch(err){
+            console.log(err.message);
         }
+            
     },
-    async update(request, response, next) {
-        try {
+    
+    async single_user(request, response) {
+            try {
+                const { user_id } = request.params;
+                let properties = await knex.from('properties')
+                    .where({ user_id })
+                return response.json(properties);
+            } catch (error) {
+                return response.json(error);
+            }
+        },
+    async create(request, response, next) {
             const {
                 property_type_id,
                 category_id,
@@ -133,15 +58,24 @@ module.exports = {
                 garage_quantity,
                 property_adress,
                 property_country,
+                property_city,
                 property_state,
                 property_neighborhood,
                 with_furniture,
                 accepts_pets,
             } = request.body;
-            const { id } = request.params;
-            await knex('properties')
-                .update(
-                    {
+            let users = await knex.from('users')
+                .where('id', user_id);
+            if (users.length == 0) {
+                return response.
+                    status(400)
+                    .json({
+                        error: true,
+                        message: 'Usuário inválido'
+                    });
+            } else {
+                try {
+                    let teste = await knex('properties').insert({
                         property_type_id,
                         category_id,
                         user_id,
@@ -153,32 +87,110 @@ module.exports = {
                         garage_quantity,
                         property_adress,
                         property_country,
-                        property_state,
                         property_neighborhood,
+                        property_city,
+                        property_state,
                         with_furniture,
-                        accepts_pets
+                        accepts_pets,
                     })
-                .where({ id });
-            return response.send();
-        } catch (error) {
-            console.log(error);
-            return response.json({
-                error: error.name,
-                message: error.message,
-            })
-        }
-    },
-    async delete(request, response, next) {
-        try {
-            const { id } = request.params;
-            await knex('properties')
-                .where({ id })
-                .del()
-            return response.send();
-        } catch (error) {
-            next(error)
+                        .returning('id')
+                        .then(async id => {
+                            for (let i = 0; i < request.files.length; i++) {
+                                try {
+                                    const url = await imgur.uploadFile(`./tmp/uploads/${request.files[i].filename}`);
+                                    await knex('files').insert({
+                                        property_id: id[0],
+                                        url: url.link,
+                                    })
+                                    if (request.files[0]) {
+                                        try {
+                                            await knex('properties').where('id', '=', id[0]).update({
+                                                ad_image: url.link
+                                            })
+                                        } catch (err) {
+                                            console.log(error);
+                                        }
+                                    };
+                                    fs.unlinkSync(`./tmp/uploads/${request.files[i].filename}`);
+                                } catch (error) {
+                                    return response.status(400).json({
+                                        message: error.message,
+                                    });
+                                }
+                            }
+                            return response.status(201).json({
+                                property_id: id[0],
+                                message: 'Cadastro com sucesso!'
+                            });
+                        });
+                } catch (error) {
+                    return response.json({
+                        message: error.message,
+                    })
+                }
+
+            }
+        },
+    async update(request, response, next) {
+            try {
+                const {
+                    property_type_id,
+                    category_id,
+                    user_id,
+                    ad_title,
+                    ad_description,
+                    ad_value,
+                    room_quantity,
+                    bathroom_quantity,
+                    garage_quantity,
+                    property_adress,
+                    property_country,
+                    property_state,
+                    property_neighborhood,
+                    with_furniture,
+                    accepts_pets,
+                } = request.body;
+                const { id } = request.params;
+                await knex('properties')
+                    .update(
+                        {
+                            property_type_id,
+                            category_id,
+                            user_id,
+                            ad_title,
+                            ad_description,
+                            ad_value,
+                            room_quantity,
+                            bathroom_quantity,
+                            garage_quantity,
+                            property_adress,
+                            property_country,
+                            property_state,
+                            property_neighborhood,
+                            with_furniture,
+                            accepts_pets
+                        })
+                    .where({ id });
+                return response.send();
+            } catch (error) {
+                console.log(error);
+                return response.json({
+                    error: error.name,
+                    message: error.message,
+                })
+            }
+        },
+    async delete (request, response, next) {
+            try {
+                const { id } = request.params;
+                await knex('properties')
+                    .where({ id })
+                    .del()
+                return response.send();
+            } catch (error) {
+                next(error)
+            }
         }
     }
-}
 
 
