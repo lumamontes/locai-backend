b5dc90ef6696107201ad88b1a699e95c4268e3a3
Alessandro Rodrigues da Silva <ale.silva1120@gmail.com>
2022-04-14 08:12:38 -0300

    heroku hot fix

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     client: 'pg',
     connection: {
       host : process.env.PG_HOST,
-      // port : 3306,
+      port : process.env.PG_PORT,
       user : process.env.PG_USER,
       password : process.env.PG_PASSWORD,
       database : process.env.PG_DATABASE,
@@ -33,14 +33,20 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host : process.env.PG_HOST,
+      port : process.env.PG_PORT,
+      user : process.env.PG_USER,
+      password : process.env.PG_PASSWORD,
+      database : process.env.PG_DATABASE,
+      charset: 'utf8'
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: __dirname + '/database/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/database/seeds',
     }
 
   }
