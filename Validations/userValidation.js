97ddbeb48ef8395501811f97980246cd88ce9ff3
Luma Montes <lumagoesmontes@gmail.com>
2022-05-09 19:30:05 -0300

    ajustado validacao

--- Validations/userValidation.js
+++ Validations/userValidation.js
@@ -5,7 +5,7 @@ const userSchema = yup.object({
 	name: yup.string().min(5).required(),                    
 	email: yup.string().email().required(),                             
 	telephone: yup.string(),                              
-	birth_date: yup.date(),                              
+	birth_date: yup.date().nullable(),                              
 	national_register: yup.string().nullable(),                           
 	city: yup.string().nullable(),                              
 	state: yup.string().nullable(),                              
