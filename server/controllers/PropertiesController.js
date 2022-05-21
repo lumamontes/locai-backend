436025b41f9af43c7b3ea47430124a48a7d4362a
Luma Montes <lumagoesmontes@gmail.com>
2022-05-21 07:27:49 -0300

    melhorado consulta de property para retornar detalhes do usuario

--- server/controllers/PropertiesController.js
+++ server/controllers/PropertiesController.js
@@ -5,60 +5,24 @@ const knex = require('../../database/knex');
 
 module.exports = {
     async index(request, response) {
-        try {
-            if (Object.keys(request.query).length !== 0 && Object.values(request.query).length !== 0) {
-                const keys = []
-                const KeyValues = []
-                for (const key in request.query) {
-                    if (request.query.hasOwnProperty.call(request.query, key)) {
-                        const element = request.query[key];
-                        if (element.length !== 0) {
-                            keys.push(key)
-                            KeyValues.push(element)
-                        }
-                    }
-                }
-                if (keys.length !== 0) {
-                    const properties = await knex.from('properties').whereIn(
-                        keys, [KeyValues]
-                    )
-                    return response.json(properties);
-                } else {
-                    let properties = await knex.from('properties');
-                    return response.json(properties);
-                }
-            } else {
-                let properties = await knex.from('properties');
-                return response.json(properties);
-            }
-        } catch (error) {
-            return response.json(error);
-        }
+
+        let properties = await knex.from('properties');
+        return response.json(properties);
+
     },
     async single_read(request, response) {
         try {
             const { id } = request.params;
-            await knex.select()
-                .from('properties').where({ id })
-                .then(async function (properties) {
-                    await knex.select('url')
-                        .from('files').where('property_id', id)
-                        .then(function (files) {
-                            response.json({
-                                property: properties,
-                                files: files,
-                            });
-                        });
-                }).catch(function (error) {
-                    return response.
-                        status(404)
-                        .json({
-                            error: true,
-                            code: 'property.invalid',
-                            message: 'ímovel não encotrado'
-                        });
-                });
+            const property = await knex.select().from('properties').where({id}).first();
+            const user = await knex.select('name', 'email', 'national_register', 'telephone').from('users').where('id', `${property.user_id}`);
+            const files = await knex.select().from('files').where('property_id', id);
+            response.json({
+                    property, 
+                    user, 
+                    files
+            })
         } catch (err) {
+            console.log(err)
             return response.
                 status(404)
                 .json({
@@ -116,8 +80,8 @@ module.exports = {
         } else {
             try {
                 const files = request.files
-                const firstFile = files.length > 0 ? request.files?.shift() : [] 
-                if(firstFile.length > 0){
+                const firstFile = files.length > 0 ? request.files?.shift() : []
+                if (firstFile.length > 0) {
                     url = await imgur.uploadFile(`./tmp/uploads/${firstFile.filename}`);
                 }
                 let teste = await knex('properties').insert({
@@ -141,7 +105,7 @@ module.exports = {
                     year_constructed,
                     property_area,
                     land_area,
-                    ad_image: firstFile.length > 0 ? url.link : null 
+                    ad_image: firstFile.length > 0 ? url.link : null
                 })
                     .returning('id')
                     .then(async id => {
@@ -159,9 +123,9 @@ module.exports = {
                                 });
                             }
                         }
-                        if(firstFile.length > 0){
+                        if (firstFile.length > 0) {
                             fs.unlinkSync(`./tmp/uploads/${firstFile.filename}`);
-                        } 
+                        }
                         return response.status(201).json({
                             property_id: id[0],
                             message: 'Cadastro com sucesso!'
